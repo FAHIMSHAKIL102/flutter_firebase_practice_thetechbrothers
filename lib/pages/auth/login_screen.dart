b3789b70db9d4eb4df1f960c607a8f33436f1873
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2026-01-28 23:10:12 +0600

    add_roundbutton

--- lib/pages/auth/login_screen.dart
+++ lib/pages/auth/login_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_practice_thetechbrothers/widgets/round_button.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Login')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: Column(
+        mainAxisAlignment: .center,
+        crossAxisAlignment: .center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: RoundButton(title: 'Login', onTap: () {}),
+          ),
+        ],
+      ),
+    );
   }
 }
