8bad5139fc2417de9cf23d1584bfd9b37a5a0ee0
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2026-04-21 21:09:34 +0600

    fix: steambuilder

--- lib/pages/post_screen.dart
+++ lib/pages/post_screen.dart
@@ -129,37 +129,37 @@ class _PostScreenState extends State<PostScreen> {
               },
             ),
            ),
-          // Expanded(
-          //   child: StreamBuilder(
-          //     stream: ref.onValue,
-          //     builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
-          //       if (!snapshot.hasData) {
-          //         return Center(
-          //           child: SizedBox(
-          //             height: 20,
-          //             width: 20,
-          //             child: CircularProgressIndicator(),
-          //           ),
-          //         );
-          //       } else {
-          //         Map<dynamic, dynamic> map =
-          //             snapshot.data!.snapshot.value as dynamic;
-          //         List<dynamic> list = [];
-          //         list.clear();
-          //         list = map.values.toList();
-          //         return ListView.builder(
-          //           itemBuilder: (context, index) {
-          //             final item = list[index];
-          //             final title = item['title']?.toString() ?? '';
-          //             final id = item['id']?.toString() ?? '';
-          //             return ListTile(title: Text(title), subtitle: Text(id));
-          //           },
-          //           itemCount: snapshot.data!.snapshot.children.length,
-          //         );
-          //       }
-          //     },
-          //   ),
-          // ),
+          Expanded(
+            child: StreamBuilder(
+              stream: ref.onValue,
+              builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: SizedBox(
+                      height: 20,
+                      width: 20,
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                } else {
+                  Map<dynamic, dynamic> map =
+                      snapshot.data!.snapshot.value as dynamic;
+                  List<dynamic> list = [];
+                  list.clear();
+                  list = map.values.toList();
+                  return ListView.builder(
+                    itemBuilder: (context, index) {
+                      final item = list[index];
+                      final title = item['title']?.toString() ?? '';
+                      final id = item['id']?.toString() ?? '';
+                      return ListTile(title: Text(title), subtitle: Text(id));
+                    },
+                    itemCount: snapshot.data!.snapshot.children.length,
+                  );
+                }
+              },
+            ),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
